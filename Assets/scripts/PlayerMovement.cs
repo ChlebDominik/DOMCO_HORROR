18377aa6227e5449cfbbc7715670e67d9d169100
ChlebDominik <119115756+ChlebDominik@users.noreply.github.com>
2024-10-03 11:17:21 +0200

    Sprint

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float walkSpeed = 45f;
+    public float sprintSpeed = 55f; // Fixed the missing semicolon
     [HideInInspector] public Rigidbody rb;
     [HideInInspector] public Vector3 moveDir;
     public Transform orientation;
-
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
+
     private void FixedUpdate()
     {
+        // Get movement direction
         moveDir = (orientation.right * Input.GetAxisRaw("Horizontal") + orientation.forward * Input.GetAxisRaw("Vertical")).normalized;
 
+        // Determine speed based on sprint input
+        float speed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed;
 
-        rb.AddForce(moveDir * 100 * walkSpeed * Time.fixedDeltaTime);
+        // Apply force to the Rigidbody
+        rb.AddForce(moveDir * 100 * speed * Time.fixedDeltaTime);
     }
 }
